0aa64d32a22cb35578db8246794d8548973bf78a
Kenzie Bollon-Kelsey <m.bollon01@pudseygrangefield.co.uk>
2019-10-17 12:22:07 +0100

    v 0.2 added age input and check.

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ print ("Ben Wallace
 name = input("Are you Ben Wallace, of course you're not, shut up, whats your name?")
 
 print (f"Hello {name}, i dont like you")
+
+age = int(input("How old are you?"))
+
+if age >+ 18:
+	print("You may visit Ben Wallace")
+else:
+	print("NO! STAY AWAY FROM BENJAMIN!")
